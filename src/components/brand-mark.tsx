75cb02be02468ef2a0cf5793569339cff9f36c8e
Bufib <bufib.bufib@gmail.com>
2026-08-21 00:59:59 +0200

    fix

--- src/components/brand-mark.tsx
+++ src/components/brand-mark.tsx
@@ -92,7 +92,7 @@ export function BrandMark({
     />
   ) : (
     <Image
-      style={{ width: 100, height: 100 }}
+      style={{ width: 90, height: 90 }}
       contentFit="contain"
       source={require("@/assets/images/logo.png")}
     />
